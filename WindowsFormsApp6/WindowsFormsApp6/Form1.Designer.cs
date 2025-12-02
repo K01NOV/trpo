01bf3a9ec093d34fe0525e212bdd96d1a441e73b
K01NOV <cubegreen7@gmail.com>
2025-12-03 01:01:31 +0700

    Task1

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pass = new System.Windows.Forms.Label();
+            this.amount = new System.Windows.Forms.TextBox();
+            this.latin = new System.Windows.Forms.TextBox();
+            this.digits = new System.Windows.Forms.TextBox();
+            this.special = new System.Windows.Forms.TextBox();
+            this.gen = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.Button();
+            this.copy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pass
+            // 
+            this.pass.AutoSize = true;
+            this.pass.Location = new System.Drawing.Point(12, 24);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(35, 13);
+            this.pass.TabIndex = 0;
+            this.pass.Text = "label1";
+            // 
+            // amount
+            // 
+            this.amount.Location = new System.Drawing.Point(15, 77);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(70, 20);
+            this.amount.TabIndex = 1;
+            this.amount.Text = "0";
+            this.amount.Leave += new System.EventHandler(this.amount_Leave);
+            // 
+            // latin
+            // 
+            this.latin.Location = new System.Drawing.Point(15, 120);
+            this.latin.Name = "latin";
+            this.latin.Size = new System.Drawing.Size(70, 20);
+            this.latin.TabIndex = 2;
+            this.latin.Text = "0";
+            this.latin.Leave += new System.EventHandler(this.latin_Leave);
+            // 
+            // digits
+            // 
+            this.digits.Location = new System.Drawing.Point(15, 169);
+            this.digits.Name = "digits";
+            this.digits.Size = new System.Drawing.Size(70, 20);
+            this.digits.TabIndex = 3;
+            this.digits.Text = "0";
+            this.digits.Leave += new System.EventHandler(this.digits_Leave);
+            // 
+            // special
+            // 
+            this.special.Location = new System.Drawing.Point(15, 222);
+            this.special.Name = "special";
+            this.special.Size = new System.Drawing.Size(70, 20);
+            this.special.TabIndex = 4;
+            this.special.Text = "0";
+            this.special.Leave += new System.EventHandler(this.special_Leave);
+            // 
+            // gen
+            // 
+            this.gen.Location = new System.Drawing.Point(15, 276);
+            this.gen.Name = "gen";
+            this.gen.Size = new System.Drawing.Size(122, 23);
+            this.gen.TabIndex = 5;
+            this.gen.Text = "generate password";
+            this.gen.UseVisualStyleBackColor = true;
+            this.gen.Click += new System.EventHandler(this.gen_Click);
+            // 
+            // list
+            // 
+            this.list.Location = new System.Drawing.Point(15, 361);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(122, 23);
+            this.list.TabIndex = 6;
+            this.list.Text = "see list of passwords";
+            this.list.UseVisualStyleBackColor = true;
+            // 
+            // copy
+            // 
+            this.copy.Location = new System.Drawing.Point(15, 320);
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(122, 23);
+            this.copy.TabIndex = 7;
+            this.copy.Text = "copy password";
+            this.copy.UseVisualStyleBackColor = true;
+            this.copy.Click += new System.EventHandler(this.copy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 450);
+            this.Controls.Add(this.copy);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.gen);
+            this.Controls.Add(this.special);
+            this.Controls.Add(this.digits);
+            this.Controls.Add(this.latin);
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.pass);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.TextBox amount;
+        private System.Windows.Forms.TextBox latin;
+        private System.Windows.Forms.TextBox digits;
+        private System.Windows.Forms.TextBox special;
+        private System.Windows.Forms.Button gen;
+        private System.Windows.Forms.Button list;
+        private System.Windows.Forms.Button copy;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        private Form2 listFormInstance = null;
+        public int checkNegative(string text)
+        {
+            if (int.TryParse(text, out int value))
+            {
+                return Math.Max(0, value);
+            }
+            return 0;
+        }
+
+        public string less50(TextBox textBox)
+        {
+            int.TryParse(textBox.Text, out int amount);
+            if (amount > 50)
+            {
+                amount = 0;
+            }
+            return amount.ToString();
+        }
+
+        public void updateData()
+        {
+            int latinCount = checkNegative(latin.Text);
+            int digitsCount = checkNegative(digits.Text);
+            int specialCount = checkNegative(special.Text);
+            int amountCount = latinCount + digitsCount + specialCount;
+            if(amountCount > 50)
+            {
+                amountCount = 50;
+                latinCount = 50;
+                digitsCount = 0;
+                specialCount = 0;
+            }
+
+            amount.Text = Convert.ToString(amountCount);
+            latin.Text = Convert.ToString(latinCount);
+            digits.Text = Convert.ToString(digitsCount);
+            special.Text = Convert.ToString(specialCount);
+        }
+
+        public void autofill()
+        {
+            int latinCount = checkNegative(latin.Text);
+            int digitsCount = checkNegative(digits.Text);
+            int specialCount = checkNegative(special.Text);
+            int amountCount = checkNegative(amount.Text);
+            if (latinCount == 0 && amountCount > 0)
+            {
+                latinCount = amountCount - specialCount - digitsCount;
+                latinCount = checkNegative(latinCount.ToString());
+            }
+            else if (digitsCount == 0 && amountCount > 0)
+            {
+                digitsCount = amountCount - latinCount - specialCount;
+                digitsCount = checkNegative(digitsCount.ToString());
+            }
+            else if (specialCount == 0 && amountCount > 0)
+            {
+                specialCount = amountCount - digitsCount - latinCount;
+                specialCount = checkNegative(specialCount.ToString());
+            }
+            latin.Text = Convert.ToString(latinCount);
+            digits.Text = Convert.ToString(digitsCount);
+            special.Text = Convert.ToString(specialCount);
+        }
+
+        public string generatePassword(int amount, int latin, int digits, int special)
+        {
+            string latinPattern = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string digitPattern = "0123456789";
+            string specialPattern = "[!@#$%^&*()]";
+            string password = "";
+
+            int position = 0;
+
+            char[] pass = new char[amount];
+
+            Random random = new Random();
+
+            for (int i = 0; i < latin; i++)
+            {
+                Console.WriteLine(position);
+                int index = random.Next(0, latinPattern.Length);
+                pass[position] = latinPattern[index];
+                position++;
+            }
+
+            for (int i = 0; i < digits; i++)
+            {
+                Console.WriteLine(position);
+                int index = random.Next(0, digitPattern.Length);
+                pass[position] = digitPattern[index];
+                position++;
+            }
+
+            for (int i = 0; i < special; i++)
+            {
+                Console.WriteLine(position);
+                int index = random.Next(0, specialPattern.Length);
+                pass[position] = specialPattern[index];
+                position++;
+            }
+
+            int n = pass.Length;
+            while (n > 1)
+            {
+                int k = random.Next(n--);
+                char temp = pass[n];
+                pass[n] = pass[k];
+                pass[k] = temp;
+            }
+
+            return new string(pass);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();          
+            
+        }
+
+        private void gen_Click(object sender, EventArgs e)
+        {
+            if (this.listFormInstance == null || this.listFormInstance.IsDisposed)
+            {
+                this.listFormInstance = new Form2();
+            }
+            autofill();
+            updateData();
+
+            int latinCount = checkNegative(latin.Text);
+            int digitsCount = checkNegative(digits.Text);
+            int specialCount = checkNegative(special.Text);
+            int amountCount = checkNegative(amount.Text);
+
+            string password = generatePassword(amountCount, latinCount, digitsCount, specialCount);
+            pass.Text = password;
+            this.listFormInstance.listBox1.Items.Add(password);
+            this.listFormInstance.Show();
+            this.listFormInstance.BringToFront();
+        }
+
+        private void copy_Click(object sender, EventArgs e)
+        {
+            if(pass.Text != "")
+            {
+                string password = pass.Text;
+                Clipboard.SetText(password);
+            }
+        }
+
+        private void amount_Leave(object sender, EventArgs e)
+        {
+            amount.Text = less50(amount);
+        }
+
+        private void latin_Leave(object sender, EventArgs e)
+        {
+            latin.Text = less50(latin);
+        }
+
+        private void digits_Leave(object sender, EventArgs e)
+        {
+            digits.Text = less50(digits);
+        }
+
+        private void special_Leave(object sender, EventArgs e)
+        {
+            special.Text = less50(special);
+        }
+    }
+}
